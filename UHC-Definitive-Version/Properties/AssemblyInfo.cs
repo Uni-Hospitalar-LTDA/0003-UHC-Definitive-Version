50fa04bf3f60375614bd528ef8ecd12c71a0d6b7
uniRafaelFeliciano <96743868+uniRafaelFeliciano@users.noreply.github.com>
2024-09-04 15:09:18 -0300

    Estrutura primordial de pastas

---
+++ UHC-Definitive-Version/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("UHC-Definitive-Version")]
+[assembly: AssemblyDescription("Ferramenta de controle interno do Grupo Uni, desenvolvida internamente pela equipe de T.I")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Grupo Uni")]
+[assembly: AssemblyProduct("UHC")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("UHC")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("315986a5-bae3-444e-ba39-3abf666d756f")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
