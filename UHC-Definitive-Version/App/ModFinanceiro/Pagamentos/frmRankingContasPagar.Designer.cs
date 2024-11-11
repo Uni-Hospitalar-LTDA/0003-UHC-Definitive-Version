33c3b6e2e1c9cee67fa8e27f72a06bebbc3a148e
Wendson Mayk <wendson444@gmail.com>
2024-11-11 11:00:31 -0300

    Adding Ranking Contas a Pagar

---
+++ UHC-Definitive-Version/App/ModFinanceiro/Pagamentos/frmRankingContasPagar.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace UHC3_Definitive_Version.App.ModFinanceiro.Pagamentos
+{
+    partial class frmRankingContasPagar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.lblVlrTotal = new System.Windows.Forms.Label();
+            this.txtVlrTotal = new System.Windows.Forms.TextBox();
+            this.lblMediaTotal = new System.Windows.Forms.Label();
+            this.lblMediaTop10 = new System.Windows.Forms.Label();
+            this.txtMediaTop10 = new System.Windows.Forms.TextBox();
+            this.txtMediaTotal = new System.Windows.Forms.TextBox();
+            this.dtpDatCorte = new System.Windows.Forms.DateTimePicker();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatórioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportar_excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportar_jsonToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportar_xmlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrir_ExcelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.gpbInformacoes = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.gpbInformacoes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(12, 88);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(928, 264);
+            this.dgvData.TabIndex = 2;
+            // 
+            // lblVlrTotal
+            // 
+            this.lblVlrTotal.AutoSize = true;
+            this.lblVlrTotal.Location = new System.Drawing.Point(527, 22);
+            this.lblVlrTotal.Name = "lblVlrTotal";
+            this.lblVlrTotal.Size = new System.Drawing.Size(58, 13);
+            this.lblVlrTotal.TabIndex = 14;
+            this.lblVlrTotal.Text = "Valor Total";
+            // 
+            // txtVlrTotal
+            // 
+            this.txtVlrTotal.Location = new System.Drawing.Point(530, 38);
+            this.txtVlrTotal.Name = "txtVlrTotal";
+            this.txtVlrTotal.Size = new System.Drawing.Size(108, 20);
+            this.txtVlrTotal.TabIndex = 15;
+            // 
+            // lblMediaTotal
+            // 
+            this.lblMediaTotal.AutoSize = true;
+            this.lblMediaTotal.Location = new System.Drawing.Point(404, 22);
+            this.lblMediaTotal.Name = "lblMediaTotal";
+            this.lblMediaTotal.Size = new System.Drawing.Size(63, 13);
+            this.lblMediaTotal.TabIndex = 10;
+            this.lblMediaTotal.Text = "Média Total";
+            // 
+            // lblMediaTop10
+            // 
+            this.lblMediaTop10.AutoSize = true;
+            this.lblMediaTop10.Location = new System.Drawing.Point(274, 22);
+            this.lblMediaTop10.Name = "lblMediaTop10";
+            this.lblMediaTop10.Size = new System.Drawing.Size(70, 13);
+            this.lblMediaTop10.TabIndex = 12;
+            this.lblMediaTop10.Text = "Média Top10";
+            // 
+            // txtMediaTop10
+            // 
+            this.txtMediaTop10.Location = new System.Drawing.Point(277, 38);
+            this.txtMediaTop10.Name = "txtMediaTop10";
+            this.txtMediaTop10.Size = new System.Drawing.Size(108, 20);
+            this.txtMediaTop10.TabIndex = 13;
+            // 
+            // txtMediaTotal
+            // 
+            this.txtMediaTotal.Location = new System.Drawing.Point(407, 38);
+            this.txtMediaTotal.Name = "txtMediaTotal";
+            this.txtMediaTotal.Size = new System.Drawing.Size(108, 20);
+            this.txtMediaTotal.TabIndex = 11;
+            // 
+            // dtpDatCorte
+            // 
+            this.dtpDatCorte.Location = new System.Drawing.Point(11, 59);
+            this.dtpDatCorte.Name = "dtpDatCorte";
+            this.dtpDatCorte.Size = new System.Drawing.Size(230, 20);
+            this.dtpDatCorte.TabIndex = 16;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(952, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aToolStripMenuItem
+            // 
+            this.aToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportarToolStripMenuItem,
+            this.abrirToolStripMenuItem});
+            this.aToolStripMenuItem.Name = "aToolStripMenuItem";
+            this.aToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.aToolStripMenuItem.Text = "Arquivo";
+            // 
+            // exportarToolStripMenuItem
+            // 
+            this.exportarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.relatórioToolStripMenuItem});
+            this.exportarToolStripMenuItem.Name = "exportarToolStripMenuItem";
+            this.exportarToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.exportarToolStripMenuItem.Text = "Exportar";
+            // 
+            // relatórioToolStripMenuItem
+            // 
+            this.relatórioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportar_excelToolStripMenuItem,
+            this.exportar_jsonToolStripMenuItem1,
+            this.exportar_xmlToolStripMenuItem});
+            this.relatórioToolStripMenuItem.Name = "relatórioToolStripMenuItem";
+            this.relatórioToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.relatórioToolStripMenuItem.Text = "Relatório";
+            // 
+            // exportar_excelToolStripMenuItem
+            // 
+            this.exportar_excelToolStripMenuItem.Name = "exportar_excelToolStripMenuItem";
+            this.exportar_excelToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.exportar_excelToolStripMenuItem.Text = "Excel";
+            // 
+            // exportar_jsonToolStripMenuItem1
+            // 
+            this.exportar_jsonToolStripMenuItem1.Name = "exportar_jsonToolStripMenuItem1";
+            this.exportar_jsonToolStripMenuItem1.Size = new System.Drawing.Size(102, 22);
+            this.exportar_jsonToolStripMenuItem1.Text = "JSON";
+            // 
+            // exportar_xmlToolStripMenuItem
+            // 
+            this.exportar_xmlToolStripMenuItem.Name = "exportar_xmlToolStripMenuItem";
+            this.exportar_xmlToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.exportar_xmlToolStripMenuItem.Text = "XML";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrir_ExcelToolStripMenuItem});
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // abrir_ExcelToolStripMenuItem
+            // 
+            this.abrir_ExcelToolStripMenuItem.Name = "abrir_ExcelToolStripMenuItem";
+            this.abrir_ExcelToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.abrir_ExcelToolStripMenuItem.Text = "Excel";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(247, 56);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 18;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // gpbInformacoes
+            // 
+            this.gpbInformacoes.Controls.Add(this.txtMediaTop10);
+            this.gpbInformacoes.Controls.Add(this.lblMediaTop10);
+            this.gpbInformacoes.Controls.Add(this.txtMediaTotal);
+            this.gpbInformacoes.Controls.Add(this.lblMediaTotal);
+            this.gpbInformacoes.Controls.Add(this.lblVlrTotal);
+            this.gpbInformacoes.Controls.Add(this.txtVlrTotal);
+            this.gpbInformacoes.Location = new System.Drawing.Point(12, 358);
+            this.gpbInformacoes.Name = "gpbInformacoes";
+            this.gpbInformacoes.Size = new System.Drawing.Size(928, 78);
+            this.gpbInformacoes.TabIndex = 19;
+            this.gpbInformacoes.TabStop = false;
+            this.gpbInformacoes.Text = "informações";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Selecione a data";
+            // 
+            // frmRankingContasPagar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 448);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gpbInformacoes);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.dtpDatCorte);
+            this.Controls.Add(this.dgvData);
+            this.Name = "frmRankingContasPagar";
+            this.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.StateCommon.Border.ColorAngle = 45F;
+            this.StateCommon.Border.DrawBorders = ((Krypton.Toolkit.PaletteDrawBorders)((((Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Border.Width = 5;
+            this.StateCommon.Header.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.StateCommon.Header.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.StateCommon.Header.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.Text = "frmRankingContasPagar";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.gpbInformacoes.ResumeLayout(false);
+            this.gpbInformacoes.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label lblVlrTotal;
+        private System.Windows.Forms.TextBox txtVlrTotal;
+        private System.Windows.Forms.Label lblMediaTotal;
+        private System.Windows.Forms.Label lblMediaTop10;
+        private System.Windows.Forms.TextBox txtMediaTop10;
+        private System.Windows.Forms.TextBox txtMediaTotal;
+        private System.Windows.Forms.DateTimePicker dtpDatCorte;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatórioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportar_excelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportar_jsonToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exportar_xmlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrir_ExcelToolStripMenuItem;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.GroupBox gpbInformacoes;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ UHC-Definitive-Version/App/ModFinanceiro/Pagamentos/frmRankingContasPagar.cs
@@ -0,0 +1,277 @@
+﻿using LiveCharts.Defaults;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UHC3_Definitive_Version.Configuration;
+using UHC3_Definitive_Version.Customization;
+using System.Drawing;
+
+namespace UHC3_Definitive_Version.App.ModFinanceiro.Pagamentos
+{
+    public partial class frmRankingContasPagar : CustomForm
+    {
+        /** Instance **/
+        List<Consulta> rankingContasPagar = new List<Consulta>();
+        internal class Consulta : Querys<Consulta>
+        {
+            public string codigoEntidade { get; set; }
+            public string descricaoEntidade { get; set; }
+            public string tipoEntidade { get; set; }
+            public string valorPrincipal { get; set; }
+
+            public async static Task<List<Consulta>> getAllToListAsync(DateTime date)
+            {
+                string query = $@"
+SELECT
+    [codigoEntidade] =COALESCE(Fornecedor.Codigo, Favorecido.Cod_Favore, Transportadora.Codigo) ,
+    [descricaoEntidade] = COALESCE(Fornecedor.Razao_Social, Favorecido.Des_Favore, Transportadora.Razao_Social),
+    [tipoEntidade] = CASE
+        WHEN CT.Cod_Fornec IS NOT NULL THEN 'Fornecedor'
+        WHEN CT.Cod_Favore IS NOT NULL THEN 'Favorecido'
+        ELSE 'Transportadora'
+    END,
+    [valorPrincipal] = SUM(CT.Val_Docume)
+FROM [DMD].dbo.[PAGCT] CT
+LEFT JOIN [DMD].dbo.[FAVOR] Favorecido ON Favorecido.Cod_Favore = CT.Cod_Favore
+LEFT JOIN [DMD].dbo.[FORNE] Fornecedor ON Fornecedor.Codigo = CT.Cod_Fornec
+LEFT JOIN [DMD].dbo.[TRANS] Transportadora ON Transportadora.Codigo = CT.Cod_Transp
+WHERE  
+
+/** Filtros Fixos **/
+                        (Sta_Docume NOT LIKE 'C' or Sta_Docume = 'C' AND Dat_Cancel > '{date.ToString("yyyyMMdd")}') 					
+                        AND(CT.Dat_Quitac > '{date.ToString("yyyyMMdd")}' OR not (CT.Dat_Quitac <= '{date.ToString("yyyyMMdd")}' and Sta_Docume = 'Q')) 	
+                        AND(CT.Dat_Registro <= '{date.ToString("yyyyMMdd")}' AND Convert(date, Dat_Emissa) <= '{date.ToString("yyyyMMdd")}')  			
+                        AND Num_Docume NOT LIKE '%P%'  																
+                        AND Tip_Documento not like 'OR'
+                        AND Dat_Vencim < '{date.ToString("yyyyMMdd")}'
+GROUP BY
+    CT.Cod_Fornec,
+    CT.Cod_Favore,
+    CT.Cod_Transp,
+	Fornecedor.Codigo, Favorecido.Cod_Favore, Transportadora.Codigo,
+    Transportadora.Razao_Social,
+    Fornecedor.Razao_Social,
+    Favorecido.Des_Favore
+ORDER BY 
+	valorPrincipal DESC";
+
+                return await getAllToList(query);
+            }
+        }
+
+
+
+
+        public frmRankingContasPagar()
+        {
+            InitializeComponent();
+            ConfigureFormProperties();
+            ConfigureDateTimePickerProperties();
+            ConfigureTextBoxProperties();
+            ConfigureFormEvents();
+            ConfiguraPropriedadesGrid(dgvData);
+            ConfigureButtonsEvents();
+
+            //btnFechar.toDefaultCloseButton();
+        }
+
+        /** Async Tasks **/
+        private async Task getRankingFromDB()
+        {
+            rankingContasPagar = await Consulta.getAllToListAsync(dtpDatCorte.Value);
+        }
+        private async Task filter()
+        {
+
+            await getRankingFromDB();
+            addItemsToDGV(rankingContasPagar);
+            adicionarItensNaBox(rankingContasPagar);
+
+        }
+
+        /** Sync Methods**/
+        private void ConfiguraPropriedadesGrid(DataGridView _dgv)
+        {
+            _dgv.Columns.Add("id", "Posição");
+            _dgv.Columns.Add("codigoEntidade", "Código");
+            _dgv.Columns.Add("descricaoEntidade", "Descrição");
+            _dgv.Columns.Add("tipoEntidade", "Tipo");
+            _dgv.Columns.Add("valorPrincipal", "Valor (R$)");
+
+            // Definir o tipo de dados das colunas
+            _dgv.Columns["codigoEntidade"].ValueType = typeof(int);
+            _dgv.Columns["descricaoEntidade"].ValueType = typeof(string);
+            _dgv.Columns["tipoEntidade"].ValueType = typeof(string);
+            _dgv.Columns["valorPrincipal"].ValueType = typeof(double);
+
+            // Formatar a coluna "valorPrincipal" como um valor monetário
+            //dgvData.Columns["valorPrincipal"].DefaultCellStyle.Format = "R$ #,##0.00";
+            _dgv.Columns["valorPrincipal"].DefaultCellStyle.Format = "C2";
+
+            _dgv.toDefault();
+            _dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            _dgv.MultiSelect = true;
+
+            _dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+        private void addItemsToDGV(List<Consulta> _list)
+        {
+            dgvData.Rows.Clear();
+            int x = 0;
+            foreach (var item in _list)
+            {
+                dgvData.Rows.Add(++x, Convert.ToInt32(item.codigoEntidade), item.descricaoEntidade, item.tipoEntidade, $"{Convert.ToDouble(item.valorPrincipal):C2}");
+            }
+            dgvData.AutoResizeColumns();
+        }
+       
+        private void adicionarItensNaBox(List<Consulta> consulta)
+        {
+            var values = new ChartValues<ObservableValue>();
+            var labels = new List<string>();
+
+            double totalValor = 0;
+
+            foreach (var cliente in consulta.Take(10))
+            {
+                values.Add(new ObservableValue(Convert.ToDouble(cliente.valorPrincipal)));
+                labels.Add(cliente.descricaoEntidade);
+                totalValor += Convert.ToDouble(cliente.valorPrincipal);
+            }
+
+            double mediaTotal = totalValor / consulta.Count;
+            double mediaTop10 = totalValor / 10;
+
+            txtMediaTop10.Text = $"{mediaTotal:C2}";
+            txtMediaTotal.Text = $"{mediaTop10:C2}";
+            txtVlrTotal.Text = $"{consulta.Sum(c => Convert.ToDouble(c.valorPrincipal)):C2}";
+        }
+
+    
+
+        /** Form Configuration **/
+        private void ConfigureFormProperties()
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+        private void ConfigureFormEvents()
+        {
+            this.Load += this.frmRankingContasPagar_Load;
+        }
+        private async void frmRankingContasPagar_Load(object sender, EventArgs e)
+        {
+            ConfigureDateTimePickerAttributes();
+            await filter();
+
+
+
+            ConfigureStripMenuEvents();
+        }
+
+
+        /** Configure TextBox **/
+        private void ConfigureTextBoxProperties()
+        {
+            txtMediaTotal.ReadOnly = true;
+            txtMediaTop10.ReadOnly = true;
+            txtVlrTotal.ReadOnly = true;
+
+
+            // Define o fundo para "Média Top 10 vendas" Cor chamativa para destecar os top 10 valores
+            txtMediaTop10.BackColor = Color.DodgerBlue;
+            //// Define o fundo para "Média Total" cor cinza, para demonstrar a média de valores nada chamativo
+            txtMediaTotal.BackColor = Color.DarkGray;
+            // Define o fundo para "Vlr Total" cor vende representando o dinheiro ou valores
+            txtVlrTotal.BackColor = Color.ForestGreen;
+
+            
+
+        }
+
+        /** Configure StripMenu **/
+        private void ConfigureStripMenuEvents()
+        {
+            exportar_excelToolStripMenuItem.Click += exportar_excelToolStripMenuItem_Click;
+            exportar_jsonToolStripMenuItem1.Click += exportar_jsonToolStripmenuItem_Click;            
+            exportar_xmlToolStripMenuItem.Click += exportar_xmlToolStripMenuItem_Click;
+            abrir_ExcelToolStripMenuItem.Click += abrir_excelToolStripMenuItem_Click;
+        }
+
+        private void exportar_excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dgvData.Rows.Count > 0)
+            {
+
+                //this.Cursor = Cursors.WaitCursor;
+                try
+                {
+                    if (dgvData.Rows.Count > 0)
+                    {
+                        SaveFileDialog saveFileDialog = new SaveFileDialog();
+                        saveFileDialog.Filter = "Excel File|*.xlsx";
+                        saveFileDialog.Title = "Save Excel File";
+                        saveFileDialog.FileName = $"_{DateTime.Now.ToString("ddMMyyyy_HHmm")}";
+                        saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                        saveFileDialog.RestoreDirectory = true;
+
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            RunMethodWithProgressBar((progress, cancellationToken) => Exportacao.exportarExcelComSoma(dgvData, saveFileDialog.FileName, progress, cancellationToken));
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+
+                }
+            }
+        }
+
+        private void exportar_jsonToolStripmenuItem_Click(object sender, EventArgs e)
+        {
+            Exportacao.toJson(dgvData, $"_{DateTime.Now.ToString("ddMMyyyy_HHmm")}");
+        }
+        private void exportar_xmlToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Exportacao.toXml(dgvData, $"_{DateTime.Now.ToString("ddMMyyyy_HHmm")}");
+        }
+
+
+        private void abrir_excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dgvData.Rows.Count > 0)
+                Exportacao.abrirDataGridViewEmExcel(dgvData);
+        }
+        
+
+        /** Configure DateTimePicker **/
+        private void ConfigureDateTimePickerProperties()
+        {
+            dtpDatCorte.MaximumSize = new System.Drawing.Size(dtpDatCorte.Width, dtpDatCorte.Height);
+            dtpDatCorte.MinimumSize = new System.Drawing.Size(dtpDatCorte.Width, dtpDatCorte.Height);
+        }
+        private void ConfigureDateTimePickerAttributes()
+        {
+            DateTime today = DateTime.Now;
+            DateTime firstDayOfNextMonth = new DateTime(today.Year, today.Month, 1).AddMonths(1);
+            DateTime lastDayOfMonth = firstDayOfNextMonth.AddDays(-1);
+            dtpDatCorte.Value = lastDayOfMonth;
+        }
+
+
+        private async void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            await filter();
+        }
+
+        
+        private void ConfigureButtonsEvents()
+        {
+            btnPesquisar.Click += btnPesquisar_Click;
+        }
+    }
+}
